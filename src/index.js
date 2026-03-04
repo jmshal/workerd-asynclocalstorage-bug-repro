700cad4fa4f69210a6231d236405abce89a1a5bb
Jacob Marshall <git@jacobmarshall.co>
2026-03-05 11:04:59 +1300

    Remove the need for durable objects

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import { DurableObject } from 'cloudflare:workers';
 import { AsyncLocalStorage } from 'node:async_hooks';
 
 const store = new AsyncLocalStorage();
+const cache = {};
 
-export class ExampleDO extends DurableObject {
-  cache = {};
-
-  fetch(request) {
-    const headerValue = request.headers.get('store-value');
-    this.cache[headerValue] = `value doesn't matter here`;
-    return store.run(headerValue, () => {
+export default {
+  fetch() {
+    const outerValue = crypto.randomUUID();
+    cache[outerValue] = `value doesn't matter here`;
+    return store.run(outerValue, () => {
       try {
         for (let i = 0; i < 1_000; i++) {
           const storeValue = store.getStore();
@@ -29,17 +27,5 @@ export class ExampleDO extends DurableObject {
       }
       return Response.json({ success: true });
     });
-  }
-}
-
-export default {
-  fetch(request, env) {
-    return env.EXAMPLE_DO.getByName('example').fetch(
-      new Request(request.url, {
-        headers: {
-          'store-value': 'hello world',
-        },
-      })
-    );
   },
 };
